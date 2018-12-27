29eee7c9c6b0b8b7c89434a99c9c76cb8915488e
Miguel Valero <miguel.valero@upc.edu>
2018-12-27 13:23:54 +0100

    Final lección 3

---
+++ src/app/profesor/profesor.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Persona } from '../Persona';
+
+@Component({
+  selector: 'app-profesor',
+  templateUrl: './profesor.component.html',
+  styleUrls: ['./profesor.component.css']
+})
+export class ProfesorComponent implements OnInit {
+
+  lista: Persona[] = [];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  Mostrar () {
+    this.lista[0] = new Persona ('Juan', 'JJJ', 'Profesor', 0);
+    this.lista[1] = new Persona ('Pedro', 'PPP', 'Alumno', 0);
+    this.lista[2] = new Persona ('Maria', 'MMM', 'Profesor', 0);
+    this.lista[3] = new Persona ('Lucas', 'LLL', 'Alumno', 0);
+    this.lista[4] = new Persona ('Salva', 'SSS', 'Alumno', 0);
+    this.lista[5] = new Persona ('Rocio', 'RRR', 'Alumno', 0);
+  }
+  Incrementar (nombre: string) {
+    this.lista.filter(persona => persona.nombre === nombre)[0].puntos ++;
+  }
+}
