83021887550b4b50cef94a9a01c2ff1e5851372a
Miguel Valero <miguel.valero@upc.edu>
2018-12-28 08:53:23 +0100

    Final leccion 4

--- src/app/profesor/profesor.component.ts
+++ src/app/profesor/profesor.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Persona } from '../Persona';
+import { ListaService } from '../lista.service';
 
 @Component({
   selector: 'app-profesor',
@@ -9,20 +10,32 @@ import { Persona } from '../Persona';
 export class ProfesorComponent implements OnInit {
 
   lista: Persona[] = [];
-  constructor() { }
+  nombre: string;
+  pass: string;
+  rol: string;
+  puntos: number;
+  constructor(private servicioLista: ListaService) { }
 
   ngOnInit() {
   }
 
   Mostrar () {
-    this.lista[0] = new Persona ('Juan', 'JJJ', 'Profesor', 0);
-    this.lista[1] = new Persona ('Pedro', 'PPP', 'Alumno', 0);
-    this.lista[2] = new Persona ('Maria', 'MMM', 'Profesor', 0);
-    this.lista[3] = new Persona ('Lucas', 'LLL', 'Alumno', 0);
-    this.lista[4] = new Persona ('Salva', 'SSS', 'Alumno', 0);
-    this.lista[5] = new Persona ('Rocio', 'RRR', 'Alumno', 0);
+    this.lista = this.servicioLista.Mostrar ();
   }
+
   Incrementar (nombre: string) {
-    this.lista.filter(persona => persona.nombre === nombre)[0].puntos ++;
+    this.lista = this.servicioLista.Incrementar (nombre);
+  }
+  Eliminar (nombre: string) {
+    this.lista = this.servicioLista.Eliminar (nombre);
+  }
+
+  OrdenarPuntos () {
+    this.lista = this.servicioLista.OrdenarPuntos();
+  }
+
+  Pon () {
+    this.lista = this.servicioLista.PonPersona (
+            new Persona (this.nombre, this.pass, this.rol, this.puntos));
   }
 }
